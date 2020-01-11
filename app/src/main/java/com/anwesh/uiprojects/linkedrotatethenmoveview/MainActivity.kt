0ff2009a2f86389a266d2d07e170f3cd958e0c81
Anwesh43 <anweshthecool0@gmail.com>
2020-01-11 18:05:55 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedrotatethenmoveview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedrotatethenmoveview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.anwesh.uiprojects.linkedrotatethenmoveview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.anwesh.uiprojects.linerotatethenmoveview.LineRotateThenMoveView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         LineRotateThenMoveView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
