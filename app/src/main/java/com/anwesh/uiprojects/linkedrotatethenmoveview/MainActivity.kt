e8454c35874de280f6169056fd1882443208b746
Anwesh43 <anweshthecool0@gmail.com>
2020-01-11 18:04:52 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedrotatethenmoveview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedrotatethenmoveview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedrotatethenmoveview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.linerotatethenmoveview.LineRotateThenMoveView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        LineRotateThenMoveView.create(this)
     }
 }
